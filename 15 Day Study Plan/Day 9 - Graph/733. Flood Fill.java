ea17d4256745f048493ba8debba98e9dccd3d27c
Josh Ratificar <105687297+not-joosh@users.noreply.github.com>
2023-01-20 10:10:02 +0800

    Add files via upload

---
+++ 15 Day Study Plan/Day 9 - Graph/733. Flood Fill.java
@@ -0,0 +1,66 @@
+/*
+        733. Flood Fill
+        Easy
+        6.4K
+        629
+        Companies
+        An image is represented by an m x n integer grid image where image[i][j] represents the pixel value of the image.
+
+        You are also given three integers sr, sc, and color. You should perform a flood fill on the image starting from the pixel image[sr][sc].
+
+        To perform a flood fill, consider the starting pixel, plus any pixels connected 4-directionally to the starting pixel of the same color as the starting pixel, plus any pixels connected 4-directionally to those pixels (also with the same color), and so on. Replace the color of all of the aforementioned pixels with color.
+
+        Return the modified image after performing the flood fill.
+
+        
+
+        Example 1:
+
+
+        Input: image = [[1,1,1],[1,1,0],[1,0,1]], sr = 1, sc = 1, color = 2
+        Output: [[2,2,2],[2,2,0],[2,0,1]]
+        Explanation: From the center of the image with position (sr, sc) = (1, 1) (i.e., the red pixel), all pixels connected by a path of the same color as the starting pixel (i.e., the blue pixels) are colored with the new color.
+        Note the bottom corner is not colored 2, because it is not 4-directionally connected to the starting pixel.
+        Example 2:
+
+        Input: image = [[0,0,0],[0,0,0]], sr = 0, sc = 0, color = 0
+        Output: [[0,0,0],[0,0,0]]
+        Explanation: The starting pixel is already colored 0, so no changes are made to the image.
+        
+
+        Constraints:
+
+        m == image.length
+        n == image[i].length
+        1 <= m, n <= 50
+        0 <= image[i][j], color < 216
+        0 <= sr < m
+        0 <= sc < n
+        Accepted
+        644K
+        Submissions
+        1.1M
+        Acceptance Rate
+        61.3%
+*/
+class Solution {
+    public int[][] floodFill(int[][] image, int sr, int sc, int color) {
+        int oldColor = image[sr][sc];
+        if(oldColor != color) {
+            dfs(image, sr, sc, oldColor, color);
+        }
+        return image;
+    }
+    
+    public void dfs(int[][] image, int i, int j, int oldColor, int newColor) {
+        if(i < 0 || i >= image.length || j < 0 || j >= image[i].length) return;
+        if(image[i][j] == oldColor) {
+            image[i][j] = newColor;
+            dfs(image, i-1, j, oldColor, newColor);
+            dfs(image, i+1, j, oldColor, newColor);
+            dfs(image, i, j-1, oldColor, newColor);
+            dfs(image, i, j+1, oldColor, newColor);
+        }
+    }
+}
+
